28f16222753452f3c2392e3c2e87457415722184
joakinBosch <joaquin.casanova.c@gmail.com>
2018-05-14 11:23:12 -0500

    Nuevos cambios

--- webempresarial/blog/views.py
+++ webempresarial/blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+def blog(request):
+    entradas = Post.objects.all()
+    return render(request, "blog/blog.html", {'entradas': entradas})
+    
