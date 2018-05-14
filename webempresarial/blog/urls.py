188f4dca129519a425d78182d77dc8c0d287928a
joakinBosch <joaquin.casanova.c@gmail.com>
2018-05-14 16:58:42 -0500

    add category funtionality

--- webempresarial/blog/urls.py
+++ webempresarial/blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.blog, name="entradas")
+    path('', views.blog, name="entradas"),
+    path('category/<int:category_id>/', views.category, name="category"),
 ]
